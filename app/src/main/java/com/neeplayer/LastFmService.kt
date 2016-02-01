9d220a52cad2819127677dd93234b36f5295a64f
Artem Daugel-Dauge <daugeldauge@gmail.com>
2016-02-01 23:30:12 +0300

    Add missing file

---
+++ app/src/main/java/com/neeplayer/LastFmService.kt
@@ -0,0 +1,15 @@
+package com.neeplayer
+
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+import retrofit2.http.Query
+import rx.Observable
+
+public interface LastFmService {
+
+    @GET("2.0?method=artist.getinfo&format=json")
+    fun getArtistInfo(
+            @Query("artist") artist: String,
+            @Query("api_key") apiKey: String = "76b52a83c8c82ae436524353bcea2da0"
+    ): Observable<ResponseBody>
+}

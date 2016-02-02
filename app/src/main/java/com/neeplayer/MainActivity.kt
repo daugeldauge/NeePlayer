1c029d0b17d13c7a0d56f4cc93164f1cc5be7ef3
Artem Daugel-Dauge <daugeldauge@gmail.com>
2016-02-03 00:49:26 +0300

    Move main activity into fragment

--- app/src/main/java/com/neeplayer/MainActivity.kt
+++ app/src/main/java/com/neeplayer/MainActivity.kt
@@ -1,121 +1,14 @@
 package com.neeplayer
 
 import android.app.Activity
-import android.content.SharedPreferences
-import android.graphics.drawable.AnimatedVectorDrawable
 import android.os.Bundle
-import android.provider.BaseColumns
-import android.provider.MediaStore
-import android.provider.MediaStore.Audio.ArtistColumns
-import android.support.v7.widget.LinearLayoutManager
-import android.view.Menu
-import kotlinx.android.synthetic.activity_main.*
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import org.jetbrains.anko.AnkoLogger
-import org.jetbrains.anko.startActivity
-import org.jetbrains.anko.warn
-import org.json.JSONObject
-import retrofit2.Retrofit
-import retrofit2.RxJavaCallAdapterFactory
-import rx.android.schedulers.AndroidSchedulers
-import rx.schedulers.Schedulers
-import java.util.*
 
-class MainActivity : Activity(), AnkoLogger {
-
-    private var artistImages: SharedPreferences? = null
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        artistImages = getSharedPreferences("ArtistImages", 0)
-
-        artist_list.adapter = ArtistAdapter(this, getArtistList()) {
-            startActivity<ArtistActivity>(
-                    "ARTIST_NAME" to it.name,
-                    "ARTIST_ID"   to it.id
-            )
-        }
-        artist_list.layoutManager = LinearLayoutManager(this)
-
+        fragmentManager.beginTransaction().add(R.id.fragment_container, MainFragment()).commit()
     }
-
-    fun getArtistList(): List<Artist> {
-        val httpClient = OkHttpClient.Builder()
-                .addInterceptor(
-                        HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC)
-                ).build();
-
-        val lastFm = Retrofit.Builder()
-                .baseUrl("http://ws.audioscrobbler.com")
-                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-                .client(httpClient)
-                .build().create(LastFmService::class.java)
-
-        val list = ArrayList<Artist>()
-
-        val cursor = contentResolver.query(
-                MediaStore.Audio.Artists.EXTERNAL_CONTENT_URI,
-                arrayOf(BaseColumns._ID, ArtistColumns.ARTIST, ArtistColumns.NUMBER_OF_TRACKS, ArtistColumns.NUMBER_OF_ALBUMS),
-                null,
-                null,
-                null)
-
-        if (cursor != null && cursor.moveToFirst()) {
-            do {
-                val artist = Artist(
-                        cursor.getLong(0),
-                        cursor.getString(1),
-                        cursor.getInt(2),
-                        cursor.getInt(3))
-
-                val image = artistImages?.getString(artist.name, null)
-                if (image.isNullOrEmpty()) {
-                    lastFm.getArtistInfo(artist.name)
-                            .subscribeOn(Schedulers.io())
-                            .observeOn(AndroidSchedulers.mainThread())
-                            .map {
-                                getArtistImageUrl(it.string())
-                            }
-                            .subscribe({
-                                artist.imageURL = it
-                                artistImages?.edit()?.putString(artist.name, it)?.commit()
-                            }, {
-                                warn("Couldn't retrieve artist image", it)
-                            })
-                } else {
-                    artist.imageURL = image
-                }
-
-                list.add(artist)
-            } while (cursor.moveToNext())
-
-            cursor.close()
-        }
-
-        return list
-    }
-
-    private fun getArtistImageUrl(artistJsonInfo: String): String? {
-        val images = JSONObject(artistJsonInfo).getJSONObject("artist").getJSONArray("image")
-
-        for (i in 0 until images.length()) {
-            val image = images.getJSONObject(i)
-            val size = image.getString("size")
-            if (size == "extralarge") {
-                return image.getString("#text")
-            }
-        }
-        return null
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu);
-        (menu?.findItem(R.id.now_playing_item)?.icon as AnimatedVectorDrawable).start()
-        return true
-    }
-
-
 }
---
+++ app/src/main/java/com/neeplayer/MainFragment.kt
@@ -0,0 +1,122 @@
+package com.neeplayer
+
+import android.app.Fragment
+import android.content.SharedPreferences
+import android.graphics.drawable.AnimatedVectorDrawable
+import android.os.Bundle
+import android.provider.BaseColumns
+import android.provider.MediaStore
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.*
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.jetbrains.anko.AnkoLogger
+import org.json.JSONObject
+import retrofit2.Retrofit
+import retrofit2.RxJavaCallAdapterFactory
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
+import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.warn
+import java.util.*
+
+class MainFragment : Fragment(), AnkoLogger {
+
+    private var artistImages: SharedPreferences? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        artistImages = activity.getSharedPreferences("ArtistImages", 0)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val recyclerView = view as RecyclerView
+
+        recyclerView.adapter = ArtistAdapter(activity, getArtistList()) {
+            activity.startActivity<ArtistActivity>("ARTIST_NAME" to it.name, "ARTIST_ID" to it.id)
+        }
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        inflater?.inflate(R.menu.menu_main, menu);
+        (menu?.findItem(R.id.now_playing_item)?.icon as AnimatedVectorDrawable).start()
+    }
+
+    private fun getArtistList(): List<Artist> {
+        val httpClient = OkHttpClient.Builder()
+                .addInterceptor(
+                        HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC)
+                ).build();
+
+        val lastFm = Retrofit.Builder()
+                .baseUrl("http://ws.audioscrobbler.com")
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .client(httpClient)
+                .build().create(LastFmService::class.java)
+
+        val list = ArrayList<Artist>()
+
+        val cursor = activity.contentResolver.query(
+                MediaStore.Audio.Artists.EXTERNAL_CONTENT_URI,
+                arrayOf(BaseColumns._ID, MediaStore.Audio.ArtistColumns.ARTIST, MediaStore.Audio.ArtistColumns.NUMBER_OF_TRACKS, MediaStore.Audio.ArtistColumns.NUMBER_OF_ALBUMS),
+                null,
+                null,
+                null)
+
+        if (cursor != null && cursor.moveToFirst()) {
+            do {
+                val artist = Artist(
+                        cursor.getLong(0),
+                        cursor.getString(1),
+                        cursor.getInt(2),
+                        cursor.getInt(3))
+
+                val image = artistImages?.getString(artist.name, null)
+                if (image.isNullOrEmpty()) {
+                    lastFm.getArtistInfo(artist.name)
+                            .subscribeOn(Schedulers.io())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .map {
+                                getArtistImageUrl(it.string())
+                            }
+                            .subscribe({
+                                artist.imageURL = it
+                                artistImages?.edit()?.putString(artist.name, it)?.commit()
+                            }, {
+                                warn("Couldn't retrieve artist image", it)
+                            })
+                } else {
+                    artist.imageURL = image
+                }
+
+                list.add(artist)
+            } while (cursor.moveToNext())
+
+            cursor.close()
+        }
+
+        return list
+    }
+
+    private fun getArtistImageUrl(artistJsonInfo: String): String? {
+        val images = JSONObject(artistJsonInfo).getJSONObject("artist").getJSONArray("image")
+
+        for (i in 0 until images.length()) {
+            val image = images.getJSONObject(i)
+            val size = image.getString("size")
+            if (size == "extralarge") {
+                return image.getString("#text")
+            }
+        }
+        return null
+    }
+
+}

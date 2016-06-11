8a99c92475abfb0b17fec68bdb83f633dcff1b7e
Artem Daugel-Dauge <daugeldauge@gmail.com>
2016-06-12 02:00:23 +0300

    Update databinding

--- app/src/main/java/com/neeplayer/ui/custom/BindingUtils.kt
+++ app/src/main/java/com/neeplayer/ui/custom/BindingUtils.kt
@@ -2,7 +2,6 @@ package com.neeplayer.ui.custom
 
 import android.databinding.BindingAdapter
 import android.graphics.drawable.Drawable
-import android.support.annotation.DrawableRes
 import android.support.graphics.drawable.AnimatedVectorDrawableCompat
 import android.widget.ImageView
 import android.widget.TextView
@@ -10,20 +9,16 @@ import com.bumptech.glide.Glide
 import com.neeplayer.R
 import java.util.concurrent.TimeUnit
 
-@BindingAdapter("bind:imagePath")
+@BindingAdapter("imagePath")
 fun loadImageFile(view: ImageView, path: String?) = loadImage(view, "file://" + path)
 
-@BindingAdapter("bind:imageURL")
+@BindingAdapter("imageURL")
 fun loadImage(view: ImageView, url: String?) = Glide.with(view.context).load(url).dontAnimate().into(view)
 
-@BindingAdapter("bind:drawable")
+@BindingAdapter("drawable")
 fun setDrawable(view: ImageView, drawable: Drawable) = view.setImageDrawable(drawable)
 
-@BindingAdapter("bind:animatedVectorDrawable")
-fun setAnimatedVectorDrawable(view: ImageView, name: String) =
-        view.setImageDrawable(AnimatedVectorDrawableCompat.create(view.context, R::class.java.getField(name).getInt(-1)))
-
-@BindingAdapter("bind:duration")
+@BindingAdapter("duration")
 fun setFormattedDuration(view: TextView, duration: Int) {
     val min = TimeUnit.MILLISECONDS.toMinutes(duration.toLong())
     val sec = TimeUnit.MILLISECONDS.toSeconds(duration.toLong()) - TimeUnit.MINUTES.toSeconds(min)

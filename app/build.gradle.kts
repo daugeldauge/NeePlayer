a52660ec99b76b762f80ef742039ca291ec2d397
Artem Daugel-Dauge <daugeldauge@gmail.com>
2019-10-26 15:55:53 +0300

    Complete rewriting build scripts to kotlin

---
+++ app/build.gradle.kts
@@ -0,0 +1,86 @@
+import net.rdrei.android.buildtimetracker.ReporterExtension
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-kapt")
+    id("kotlin-android-extensions")
+    id("kotlinx-serialization")
+    id("build-time-tracker")
+}
+
+android {
+    compileSdkVersion(29)
+
+    defaultConfig {
+        minSdkVersion(23)
+        targetSdkVersion(29)
+        applicationId = "com.neeplayer"
+        versionCode = 1
+        versionName = "1.0"
+
+        vectorDrawables.useSupportLibrary = true
+    }
+    dataBinding {
+        isEnabled = true
+    }
+    packagingOptions {
+        exclude("META-INF/**.kotlin_module")
+    }
+}
+
+dependencies {
+
+    val kotlinVersion = org.jetbrains.kotlin.config.KotlinCompilerVersion.VERSION
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
+
+    implementation("androidx.media:media:1.1.0")
+    implementation("androidx.recyclerview:recyclerview:1.0.0")
+    implementation("androidx.appcompat:appcompat:1.1.0")
+    implementation("com.google.android.material:material:1.0.0")
+
+    val glideVersion = "4.10.0"
+    implementation("com.github.bumptech.glide:glide:$glideVersion")
+    kapt("com.github.bumptech.glide:compiler:$glideVersion")
+
+    implementation("com.pushtorefresh.storio:content-resolver:1.8.0")
+
+    val daggerVersion = "2.24"
+    kapt("com.google.dagger:dagger-compiler:$daggerVersion")
+    implementation("com.google.dagger:dagger:$daggerVersion")
+
+    implementation("com.jakewharton.timber:timber:4.7.1")
+
+    val ktorVersion = "1.2.5"
+    implementation("io.ktor:ktor-client-android:$ktorVersion")
+    implementation("io.ktor:ktor-client-logging-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-client-serialization-jvm:$ktorVersion")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.13.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.1")
+}
+
+androidExtensions {
+    isExperimental = true
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf(
+                "-Xuse-experimental=kotlin.Experimental",
+                "-Xuse-experimental=kotlinx.coroutines.ExperimentalCoroutinesApi",
+                "-Xuse-experimental=kotlinx.coroutines.FlowPreview"
+        )
+    }
+}
+
+buildtimetracker {
+    reporters {
+        ReporterExtension("summary").apply {
+            options["threshold"] = "1000"
+            options["shortenTaskNames"] = "false"
+            add(this)
+        }
+    }
+}
+
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,25 @@
+pluginManagement {
+    val kotlinVersion = "1.3.60-eap-25"
+
+    repositories {
+        jcenter()
+        google()
+        maven(url = "https://dl.bintray.com/kotlin/kotlin-eap")
+    }
+
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.id) {
+                "com.android.application" -> "com.android.tools.build:gradle:4.0.0-alpha01"
+                "kotlin-android",
+                "kotlin-kapt",
+                "kotlin-android-extensions" -> "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion"
+                "kotlinx-serialization" -> "org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion"
+                "build-time-tracker" -> "net.rdrei.android.buildtimetracker:gradle-plugin:0.11.1"
+                else -> error("Unknown plugin id")
+            }.let(::useModule)
+        }
+    }
+}
+
 include("app")

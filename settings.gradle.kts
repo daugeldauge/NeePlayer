5a9fb8c20d723fed8dcf77b0891f300ff883e672
Artem Daugel-Dauge <daugeldauge@gmail.com>
2020-04-12 20:24:03 +0300

    Improve error message

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,14 +9,14 @@ pluginManagement {
 
     resolutionStrategy {
         eachPlugin {
-            when (requested.id.id) {
-                "com.android.application" -> "com.android.tools.build:gradle:4.0.0-beta04"
+            when (val pluginId = requested.id.id) {
+                "com.android.application", "com.android.library" -> "com.android.tools.build:gradle:4.0.0-beta04"
                 "kotlin-android",
                 "kotlin-kapt",
                 "kotlin-android-extensions" -> "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion"
                 "kotlinx-serialization" -> "org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion"
                 "build-time-tracker" -> "net.rdrei.android.buildtimetracker:gradle-plugin:0.11.1"
-                else -> error("Unknown plugin id")
+                else -> error("Unknown plugin id: '$pluginId'")
             }.let(::useModule)
         }
     }

90610ac372cd5365b109d422a1b196d575f48ba3
Artem Daugel-Dauge <daugeldauge@gmail.com>
2020-04-26 20:17:38 +0300

    Another glide image rewrite

--- compose/src/main/java/com/neeplayer/compose/ArtistsScreen.kt
+++ compose/src/main/java/com/neeplayer/compose/ArtistsScreen.kt
@@ -3,14 +3,19 @@ package com.neeplayer.compose
 import androidx.compose.Composable
 import androidx.ui.core.Modifier
 import androidx.ui.foundation.AdapterList
+import androidx.ui.foundation.Image
 import androidx.ui.foundation.Text
+import androidx.ui.graphics.Color
+import androidx.ui.graphics.painter.ColorPainter
 import androidx.ui.layout.Column
 import androidx.ui.layout.Row
 import androidx.ui.layout.padding
+import androidx.ui.layout.size
 import androidx.ui.material.MaterialTheme
 import androidx.ui.material.Surface
 import androidx.ui.material.lightColorPalette
 import androidx.ui.tooling.preview.Preview
+import androidx.ui.unit.Size
 import androidx.ui.unit.dp
 
 @Composable
@@ -28,7 +33,7 @@ fun ArtistItem(artist: Artist) {
             bottom = 5.dp,
             top = 5.dp
     )) {
-        GlideImage(artist.imageUrl)
+        GlideImage(artist.imageUrl, modifier = Modifier.size(90.dp))
         Column(modifier = Modifier.padding(all = 10.dp)) {
             Text(text = artist.name, style = MaterialTheme.typography.body1)
             Text(text = "${artist.numberOfAlbums} albums, ${artist.numberOfSongs} songs", style = MaterialTheme.typography.body2)
--- compose/src/main/java/com/neeplayer/compose/GlideImage.kt
+++ compose/src/main/java/com/neeplayer/compose/GlideImage.kt
@@ -1,60 +1,69 @@
 package com.neeplayer.compose
 
 import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import androidx.compose.Composable
 import androidx.compose.onCommit
 import androidx.compose.state
 import androidx.ui.animation.Crossfade
 import androidx.ui.core.ContextAmbient
-import androidx.ui.core.DensityAmbient
 import androidx.ui.core.Modifier
+import androidx.ui.core.WithConstraints
 import androidx.ui.foundation.Image
-import androidx.ui.graphics.ImageAsset
+import androidx.ui.graphics.Color
 import androidx.ui.graphics.asImageAsset
-import androidx.ui.layout.size
-import androidx.ui.res.imageResource
-import androidx.ui.unit.dp
+import androidx.ui.graphics.painter.ColorPainter
+import androidx.ui.graphics.painter.ImagePainter
+import androidx.ui.graphics.painter.Painter
+import androidx.ui.unit.IntPx
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.Target
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.target.Target.SIZE_ORIGINAL
+import com.bumptech.glide.request.transition.Transition
 
 @Composable
-fun GlideImage(model: Any?) {
-    val result = state<ImageAsset?> { null }
+fun GlideImage(model: Any?, modifier: Modifier = Modifier) {
+    val placeholderPainter = ColorPainter(Color.Transparent)
+    val errorPainter = ColorPainter(Color.LightGray)
+    val painter = state<Painter> { placeholderPainter }
     val context = ContextAmbient.current
-    val density = DensityAmbient.current
 
-    onCommit(model) {
-        val listener = object : RequestListener<Bitmap> {
-            override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Bitmap>, isFirstResource: Boolean): Boolean {
-                result.value = null
-                return false
-            }
-
-            override fun onResourceReady(resource: Bitmap, model: Any?, target: Target<Bitmap>, dataSource: DataSource, isFirstResource: Boolean): Boolean {
-                result.value = resource.asImageAsset()
-                return true
-            }
-        }
-
-        val glide = Glide.with(context)
-        val target = with(density) {
-            glide
+    WithConstraints(modifier) { constraints, _ ->
+        onCommit(model) {
+            val glide = Glide.with(context)
+            val target = glide
                     .asBitmap()
                     .load(model)
                     .centerCrop()
-                    .addListener(listener)
-                    .preload(90.dp.toIntPx().value, 90.dp.toIntPx().value)
+                    .override(constraints.maxWidth.glideSize, constraints.maxHeight.glideSize)
+                    .into(object : CustomTarget<Bitmap>() {
+                        override fun onLoadStarted(placeholder: Drawable?) {
+                            painter.value = placeholderPainter
+                        }
+
+                        override fun onLoadCleared(placeholder: Drawable?) {
+                            painter.value = placeholderPainter
+                        }
+
+                        override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                            painter.value = ImagePainter(resource.asImageAsset())
+                        }
+
+                        override fun onLoadFailed(errorDrawable: Drawable?) {
+                            painter.value = errorPainter
+                        }
+                    })
+
+
+            onDispose {
+                painter.value = placeholderPainter
+                glide.clear(target)
+            }
         }
 
-        onDispose {
-            glide.clear(target)
-        }
-    }
-
-    Crossfade(current = result.value) { asset ->
-        Image(asset ?: imageResource(id = android.R.drawable.star_on), modifier = Modifier.size(90.dp))
+        Crossfade(current = painter.value) { Image(it) }
     }
 }
+
+private val IntPx.glideSize
+    get() = takeIf { it != IntPx.Infinity }?.value ?: SIZE_ORIGINAL

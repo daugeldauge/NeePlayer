0e85808f9b3df84aa881f4bb04a71b608320edb0
Artem Daugel-Dauge <daugeldauge@gmail.com>
2020-04-26 19:31:55 +0300

    Add crossfade

--- compose/src/main/java/com/neeplayer/compose/ArtistsScreen.kt
+++ compose/src/main/java/com/neeplayer/compose/ArtistsScreen.kt
@@ -3,17 +3,13 @@ package com.neeplayer.compose
 import androidx.compose.Composable
 import androidx.ui.core.Modifier
 import androidx.ui.foundation.AdapterList
-import androidx.ui.foundation.Image
 import androidx.ui.foundation.Text
-import androidx.ui.graphics.asImageAsset
 import androidx.ui.layout.Column
 import androidx.ui.layout.Row
 import androidx.ui.layout.padding
-import androidx.ui.layout.preferredSize
 import androidx.ui.material.MaterialTheme
 import androidx.ui.material.Surface
 import androidx.ui.material.lightColorPalette
-import androidx.ui.res.imageResource
 import androidx.ui.tooling.preview.Preview
 import androidx.ui.unit.dp
 
@@ -32,7 +28,7 @@ fun ArtistItem(artist: Artist) {
             bottom = 5.dp,
             top = 5.dp
     )) {
-        Image(asset = glideBitmap(artist.imageUrl)?.asImageAsset() ?: imageResource(id = android.R.drawable.star_on), modifier = Modifier.preferredSize(90.dp, 90.dp))
+        GlideImage(artist.imageUrl)
         Column(modifier = Modifier.padding(all = 10.dp)) {
             Text(text = artist.name, style = MaterialTheme.typography.body1)
             Text(text = "${artist.numberOfAlbums} albums, ${artist.numberOfSongs} songs", style = MaterialTheme.typography.body2)
--- compose/src/main/java/com/neeplayer/compose/GlideImage.kt
+++ compose/src/main/java/com/neeplayer/compose/GlideImage.kt
@@ -2,9 +2,18 @@ package com.neeplayer.compose
 
 import android.graphics.Bitmap
 import androidx.compose.Composable
-import androidx.compose.onPreCommit
+import androidx.compose.onCommit
 import androidx.compose.state
+import androidx.ui.animation.Crossfade
 import androidx.ui.core.ContextAmbient
+import androidx.ui.core.DensityAmbient
+import androidx.ui.core.Modifier
+import androidx.ui.foundation.Image
+import androidx.ui.graphics.ImageAsset
+import androidx.ui.graphics.asImageAsset
+import androidx.ui.layout.size
+import androidx.ui.res.imageResource
+import androidx.ui.unit.dp
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
@@ -12,28 +21,40 @@ import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
 
 @Composable
-fun glideBitmap(model: Any?): Bitmap? {
-    val result = state<Bitmap?> { null }
+fun GlideImage(model: Any?) {
+    val result = state<ImageAsset?> { null }
     val context = ContextAmbient.current
-    onPreCommit(model) {
+    val density = DensityAmbient.current
+
+    onCommit(model) {
         val listener = object : RequestListener<Bitmap> {
-            override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Bitmap>?, isFirstResource: Boolean): Boolean {
+            override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Bitmap>, isFirstResource: Boolean): Boolean {
                 result.value = null
                 return false
             }
 
-            override fun onResourceReady(resource: Bitmap?, model: Any?, target: Target<Bitmap>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
-                result.value = resource
+            override fun onResourceReady(resource: Bitmap, model: Any?, target: Target<Bitmap>, dataSource: DataSource, isFirstResource: Boolean): Boolean {
+                result.value = resource.asImageAsset()
                 return true
             }
         }
 
-        Glide.with(context)
-                .asBitmap()
-                .load(model)
-                .addListener(listener)
-                .preload()
+        val glide = Glide.with(context)
+        val target = with(density) {
+            glide
+                    .asBitmap()
+                    .load(model)
+                    .centerCrop()
+                    .addListener(listener)
+                    .preload(90.dp.toIntPx().value, 90.dp.toIntPx().value)
+        }
 
+        onDispose {
+            glide.clear(target)
+        }
     }
-    return result.value
+
+    Crossfade(current = result.value) { asset ->
+        Image(asset ?: imageResource(id = android.R.drawable.star_on), modifier = Modifier.size(90.dp))
+    }
 }
